1676a8220ea1dc78464fc7f73c9508491957f1bf
Ashutosh Tewari <krishwave66@gmail.com>
2024-03-18 18:18:04 +0530

    sidebar and navbar user?. add

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -220,13 +220,13 @@ const Sidebar = ({
 									fontSize="0.9rem"
 									sx={{ color: theme.palette.secondary[100] }}
 								>
-									{user.fname}
+									{user?.fname}
 								</Typography>
 								<Typography
 									fontSize="0.8rem"
 									sx={{ color: theme.palette.secondary[200] }}
 								>
-									{user.city}
+									{user?.city}
 								</Typography>
 							</Box>
 							{/* <SettingsOutlined
